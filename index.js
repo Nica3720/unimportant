29c544fdeab7614220f737cbd0d714b5730d280e
derlole <122916573+derlole@users.noreply.github.com>
2023-05-16 16:02:07 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const { Client, IntentsBitField, Partials } = require("discord.js")
+const { DefaultCommands } = require("wokcommands")
+const WOK = require("wokcommands")
+const path = require("path")
+
+require('dotenv').config()
+const keepAlive = require("./server.js")
+const config = require("./config.json")
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.DirectMessages,
+    IntentsBitField.Flags.MessageContent,
+    IntentsBitField.Flags.GuildMessageReactions,
+    IntentsBitField.Flags.GuildMembers,
+  ],
+  partials: [Partials.Channel],
+})
+
+client.on("ready", async () => {
+  console.log("Ready!")
+  new WOK({
+    client,
+    commandsDir: path.join(__dirname, "commands"),
+    featuresDir: path.join(__dirname, "features"),
+    events: {
+      dir: path.join(__dirname, "events"),
+    },
+    testServers: [config.testServer],
+    botOwners: [config.ownerID, config.derloleID],
+    disabledDefaultCommands: [
+      DefaultCommands.ChannelCommand,
+      DefaultCommands.CustomCommand,
+      DefaultCommands.Prefix,
+      DefaultCommands.RequiredPermissions,
+      DefaultCommands.RequiredRoles,
+      DefaultCommands.ToggleCommand,
+    ],
+  })
+})
+
+client.login(process.env.TOKEN || process.env["Token"])
+keepAlive()
