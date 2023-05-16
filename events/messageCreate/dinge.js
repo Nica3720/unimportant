f36b9ff3226cffda2eac94d01ab902f89849d7c3
derlole <122916573+derlole@users.noreply.github.com>
2023-05-16 14:33:06 +0000

    test

---
+++ events/messageCreate/dinge.js
@@ -0,0 +1,5 @@
+module.exports = (message) => {
+    if (message.content === "Chemie" && message.channel.id === "1079807642639290510") {
+      message.reply("SCHEMY ist sehr grauenhaft und unangebracht. NEHMT EUCH VOR CHEMIKERN IN ACHT!")
+    }
+  }
