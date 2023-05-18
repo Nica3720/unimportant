2b0ca4c6a6880a0bb480af1ed3d1420be83289d7
Nica3720 <133804170+Nica3720@users.noreply.github.com>
2023-05-18 20:53:53 +0000

    ping läuft, reminder komplett am Start (Co-authored-by: derlole)

---
+++ commands/reminder.js
@@ -0,0 +1,20 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const {Client, Collection, Events, GatewayIntentBits} = require('discord.js');
+const { token } = require('./config.json');
+const client = new Client ({intents: [GatewayIntentBits.Guilds]});
+client.commands = new Collection();
+
+
+
+const {CommandType} = require("wokcommands");
+
+module.exports = {
+    description: "Test if the Bot is online :D",
+    type: CommandType.BOTH,
+    callback: (interaction) => {
+        return {
+        content: "Yeeet - ich lebe :squid:",
+        }
+},
+}
--- events/messageCreate/dinge.js
+++ events/messageCreate/dinge.js
@@ -1,5 +1,17 @@
 module.exports = (message) => {
-    if (message.content === "Chemie") {
-      message.reply("SCHEMY ist sehr grauenhaft und unangebracht. NEHMT EUCH VOR CHEMIKERN IN ACHT!")
+  const channel = message.channel
+    if (message.content.includes("Chemie")) {
+      message.reply("SCHEMY ist sehr grauenhaft und unangebracht. NEHMT EUCH VOR CHEMIKERN IN 8!")
+      if (message.content.includes("derlole")) {
+        message.reply("YeOOOOOOOOO :squid: :melting_face:")
+        }
     }
+    if (message.content === "derlole") {
+      message.reply("YeOOOOOOOOOOOOOOO :squid: :melting_face:")
+    }
+    if(false) {
+      if (message.content.includes("egal")) {
+        channel.send("**ILLEGAL!!!**")
+      }
+  }
   }
