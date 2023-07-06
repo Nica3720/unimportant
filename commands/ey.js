c86bd3c8362dbd312edcd7ff9faa31249b621820
Nica3720 <133804170+Nica3720@users.noreply.github.com>
2023-07-06 17:55:17 +0000

    yee

--- commands/ey.js
+++ commands/ey.js
@@ -3,6 +3,7 @@ const {CommandType} = require("wokcommands");
 module.exports = {
     description: "Illegalität betonen",
     type: CommandType.BOTH,
+    testonly: false,
     callback: (interaction) => {
         return {
         content: "# __**DAS IST VOLL ILLEGAL!!!! :(**__ \n (mit anwältlicher Bestätigung von nica3720)",
