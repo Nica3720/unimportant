1626d21de18612aeee5036ab181a76081ba371bc
einswyldesacin <133804170+Nica3720@users.noreply.github.com>
2023-08-22 10:46:31 +0000

    hiermussleben

--- commands/reminder.js
+++ commands/reminder.js
@@ -1,7 +1,8 @@
 const {CommandType} = require('wokcommands')
+testOnly = true
 
 function formatMinutes(milliseconds) {
-    if (!milliseconds) return 'Ungültige Eingabe'
+    if (!milliseconds) return ':grey_exclamation: Ungültige Eingabe'
     let seconds = Math.floor(milliseconds / 1000) % 60
     let minutes = Math.floor(milliseconds / 60000)
     return `${minutes.toString()}:${seconds.toString().padStart(2, '0')} min`
@@ -52,31 +53,57 @@ module.exports = {
             description: 'repeating the reminder command',
             type: 3,
             required: false,
-        },
-    ],
-    testOnly:true,
-
-    
-
+        },],
+    testOnly: true,
     callback: ({ interaction, args }) => {
         //Botaktion
-        console.log(interaction.options.getString('after')) //bei 'get' immer type hinschreiben
+        //console.log(interaction.options.getString('after')) //bei 'get' immer type hinschreiben
         const time = interaction.options.getString('time')
+        const [hours, minutes, seconds] = time.split(':'); // 'hh:mm:ss' aufteilen
+        const timeAfterMilliseconds = (parseInt(hours) * 3600 + parseInt(minutes) * 60 + parseInt(seconds)) * 1000; // In Millisekunden umrechnen
+        //const alarmDateTime = new Date();
+            //alarmDateTime.setFullYear(2023);
+            //alarmDateTime.setMonth(9); //Null-basiert; Sprich: Januar = Monat 0
+            //alarmDateTime.setDate(18);
+            //alarmDateTime.setHours(12);
+            //alarmDateTime.setMinutes(11);
+            //alarmDateTime.setSeconds(0);
+        //const currentDateTime = new Date();
+        //const timeUntilAlarm = alarmDateTime - currentDateTime;
         const after = parseInt(interaction.options.getString('after')); // Convert time to milliseconds
-        const channel = interaction.options.getString('channel')
-        const message = interaction.options.getString('message')
-        const date = interaction.options.getString('date')
-        const user = interaction.options.getUser('user')
-        const interval = interaction.options.getString('interval')
+        const channel = interaction.options.getString('channel');
+        const message = interaction.options.getString('message');
+        const date = interaction.options.getString('date');
+        const user = interaction.options.getUser('user');
+        const interval = interaction.options.getString('interval');
         //const reminder = args.slice(1).join(' ');
 
-        if (!isNaN(after) /*&& reminder*/) {
-            setTimeout(() => {
-            interaction.channel.send(`Erinnerung: ${message}`)
-            }, after);
-            interaction.channel.send(`Erinnerung gesetzt in: ${formatMinutes(after)}.`);
+        if (time || after) {
+            if (!isNaN(after) /*&& reminder*/) {
+                setTimeout(() => {
+                interaction.channel.send(`Erinnerung: ${message}`)
+                }, after);
+                interaction.channel.send(`Erinnerung gesetzt in: ${formatMinutes(after)}.`);
+            } else if (after !== null) {
+                interaction.channel.send(`:grey_exclamation: Ungültige Eingabe?`);
+            }
+            if (time) {
+                console.log(interaction.options.getString('time'))
+                if (timeAfterMilliseconds > 0) {
+                    console.log(interaction.options.getString('time'))
+                    setTimeout(() => {
+                        interaction.channel.send(`Erinnerung: ${message}`);
+                    }, timeAfterMilliseconds);
+            }} else if (time !== null) {
+                (':grey_exclamation: Bitte verwende eine Zeit, die in der Zukunft liegt.')
+            }
+                //if (currentDateTime.getTime() < alarmDateTime.getTime()) {
+                    //setTimeout(() => {
+                        //interaction.channel.send(`Erinnerung: ${message}`)
+                    //}, timeUntilAlarm);
+                //} else {
+                    //interaction.channel.send(`! Bitte verwende eine Zeit, die in der Zukunft liegt.`)
         } else {
-            interaction.channel.send(`! Ungültige Eingabe.`);
-        }
-    }
-}
+            return {
+                content: "Bitte gib 'after' oder 'time' an, um einen reminder zu setzen."}
+            }}}
